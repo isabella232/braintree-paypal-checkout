940faa31a74b2ec8c54d6a9c7181f15b85b0ca59
Sunny <hitse@paypal.com>
2020-08-25 18:21:28 +0800

    init proj

---
+++ routes/checkout.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment:  braintree.Environment.Sandbox,
+    merchantId:   'fgbw23g9cwm847px',
+    publicKey:    'nygrcphd83kgqj6w',
+    privateKey:   '8d8f2a43045fa17464c24674981aadd3'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
